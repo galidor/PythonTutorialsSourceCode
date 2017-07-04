31af4d065a8421b3f0da4066ff73af6f73084926
galidor <galidor94@gmail.com>
2017-07-04 22:38:13 +0200

    First bunch of simple exercises.

--- FunctionTest.py
+++
@@ -1,6 +0,0 @@
-def girls_range(age):
-    max_age = age/2 + 7
-    return max_age
-
-for i in range(1,51):
-    print("You are ", i, ".\n","You can date girls at the age of ", girls_range(i), " or older. ;)")
